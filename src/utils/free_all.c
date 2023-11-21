7ccbed73678e4cdbaca8060b580357530cd24eb7
Mini <wndavey@hotmail.com>
2023-11-21 17:34:30 +1030

    ft_free_all -> free_all

--- src/utils/free_all.c
+++ src/utils/free_all.c
@@ -6,13 +6,13 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 06:40:19 by wdavey            #+#    #+#             */
-/*   Updated: 2023/10/26 16:11:59 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/11/21 17:28:03 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	ft_free_all(char **strs)
+void	free_all(char **strs)
 {
 	size_t	iii;
 	char	*now;
