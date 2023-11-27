fa55287077822b63f993e5b72c54d07f70c8c879
Mini <wndavey@hotmail.com>
2023-11-27 19:28:08 +1030

    change builtin_cd to fit

--- include/r_builtin.h
+++ include/r_builtin.h
@@ -6,13 +6,15 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:55:50 by wdavey            #+#    #+#             */
-/*   Updated: 2023/11/27 18:55:55 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/11/27 19:20:51 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef R_BUILTIN_H
 # define R_BUILTIN_H
 
-void	builtin_cd(char *path);
+# include "command.h"
+
+int	builtin_cd(t_command cmd);
 
 #endif
--- src/r_builtin/r_builtin_cd.c
+++ src/r_builtin/r_builtin_cd.c
@@ -6,15 +6,18 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:37:29 by wdavey            #+#    #+#             */
-/*   Updated: 2023/11/27 18:41:40 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/11/27 19:21:09 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "r_builtin.h"
 
-void	builtin_cd(char *path)
+int	builtin_cd(t_command cmd)
 {
+	char	*path;
+
+	path = cmd.argv[1];
 	if (path == NULL)
 	{
 		path = getenv("HOME");
@@ -27,5 +30,7 @@ void	builtin_cd(char *path)
 	if (chdir(path) != 0)
 	{
 		perror("cd");
+		return (1);
 	}
+	return (0);
 }
