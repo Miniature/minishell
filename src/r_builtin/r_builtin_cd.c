f550511f7c8a5e0692bcfaeb55097bffa6e654b1
Mini <wndavey@hotmail.com>
2023-11-27 18:57:40 +1030

    clean up after reuben :)

--- src/r_builtin/r_builtin_cd.c
+++ src/r_builtin/r_builtin_cd.c
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   r_builtin.h                                        :+:      :+:    :+:   */
+/*   r_builtin_cd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/27 18:55:50 by wdavey            #+#    #+#             */
-/*   Updated: 2023/11/27 18:55:55 by wdavey           ###   ########.fr       */
+/*   Created: 2023/11/27 18:37:29 by wdavey            #+#    #+#             */
+/*   Updated: 2023/11/27 18:41:40 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef R_BUILTIN_H
-# define R_BUILTIN_H
+#include <stddef.h>
+#include "r_builtin.h"
 
-void	builtin_cd(char *path);
-
-#endif
+void	builtin_cd(char *path)
+{
+	if (path == NULL)
+	{
+		path = getenv("HOME");
+	}
+	else if (!strcmp(path, "-"))
+	{
+		path = getenv("OLDPWD");
+		printf("%s\n", path);
+	}
+	if (chdir(path) != 0)
+	{
+		perror("cd");
+	}
+}
