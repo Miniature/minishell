4541c775f4d0e4c1cbfdc831f2402f2597dce0b9
Mini <wndavey@hotmail.com>
2023-11-28 16:35:52 +1030

    env manipulation needs env

--- include/env.h
+++ include/env.h
@@ -6,14 +6,14 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 16:26:42 by wdavey            #+#    #+#             */
-/*   Updated: 2023/11/28 16:27:31 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/11/28 16:35:33 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
-char	*ms_getenv(char *name);
-void	ms_setenv(char *value);
+char	*ms_getenv(char ***envp, char *name);
+void	ms_setenv(char ***envp, char *value);
 
 #endif
