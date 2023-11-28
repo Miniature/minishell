a5397fd037ec3e638c6d5225c7eba369519ffb33
Mini <wndavey@hotmail.com>
2023-11-28 16:36:48 +1030

    update cd for new env function prototypes

--- src/r_builtin/r_builtin_cd.c
+++ src/r_builtin/r_builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 18:37:29 by wdavey            #+#    #+#             */
-/*   Updated: 2023/11/28 16:33:32 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/11/28 16:36:36 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int	builtin_cd(t_command cmd)
 	path = cmd.argv[1];
 	if (path == NULL)
 	{
-		path = ms_getenv("HOME");
+		path = ms_getenv(cmd.envp, "HOME");
 	}
 	else if (!ft_strncmp(path, "-", -1))
 	{
-		path = ms_getenv("OLDPWD");
+		path = ms_getenv(cmd.envp, "OLDPWD");
 		printf("%s\n", path);
 	}
 	if (chdir(path) != 0)
