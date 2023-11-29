99f38043cae5667df7a81d228956b0ff65e9da91
Mini <wndavey@hotmail.com>
2023-11-29 17:45:16 +1030

    make copy of envp at start so its not in static memory

--- src/main.c
+++ src/main.c
@@ -13,14 +13,37 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "r_signal.h"
+#include <stdlib.h>
+#include "utils.h"
 
 t_signal	g_signal;
+
+//envp gets edited, so it needs to not be in static memory
+static char	**copy_envp(char **envp)
+{
+	char	**copy;
+	size_t	size;
+
+	size = 0;
+	while (envp[size])
+		size++;
+	copy = malloc(sizeof(*copy) * (size + 1));
+	while (size != -1UL)
+	{
+		copy[size] = NULL;
+		if (envp[size])
+			copy[size] = ft_strdup(envp[size]);
+		size--;
+	}
+	return (copy);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
 	(void)envp;
+	envp = copy_envp(envp);
 	char buf[129];
 	buf[read(0, buf, 128)] = 0;
 		printf("minishell: %s\n", buf);
@@ -29,5 +52,6 @@ int	main(int argc, char **argv, char **envp)
 		signal(SIGINT, &signal_handler);
 		signal(SIGQUIT, &signal_handler);
 	}
+	free_all(envp);
 	return (0);
 }
