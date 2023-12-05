775371f41f683eebbfcb100e1665b348a6b53ce6
Mini <wndavey@hotmail.com>
2023-12-05 14:09:10 +1030

    back to builtin before fork

--- src/exec/exec_command_builtin.c
+++ src/exec/exec_command_builtin.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 19:49:36 by wdavey            #+#    #+#             */
-/*   Updated: 2023/12/04 19:05:35 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/12/05 14:04:54 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,10 @@ int	exec_command_builtin(t_command cmd, int (*f)(t_command))
 	int		rval;
 	pid_t	pid;
 
+	rval = f(cmd);
 	pid = fork();
 	if (0 == pid)
 	{
-		rval = 1;
-		if (-1 == dup2(cmd.fd[FD_IN], STDIN_FILENO))
-		{
-			perror("minishell: exec");
-		}
-		else if (-1 == dup2(cmd.fd[FD_OUT], STDOUT_FILENO))
-		{
-			perror("minishell: exec");
-		}
-		else
-			rval = f(cmd);
 		exit(rval);
 	}
 	return (pid);
