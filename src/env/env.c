d815171d7deb20c7168645c1a279812aa43c6ecb
Reuben Kabzinski <rkabzins@student.42adel.org.au>
2023-12-05 18:30:47 +1030

    changed stderr to STDERR_FILENO

--- src/env/env.c
+++ src/env/env.c
@@ -52,7 +52,7 @@ void	ms_setenv(char ***envp, char *value)
 	equal = ft_strchr(value, '=');
 	if (equal == NULL)
 	{
-		write(stderr, "Error: Invalid Environment Variable\n", 36);
+		write(STDERR_FILENO, "Error: Invalid Environment Variable\n", 36);
 		return ;
 	}
 	name_length = equal - value;
@@ -77,8 +77,8 @@ void	ms_unsetenv(char ***envp, char *name)
 	new_env = realloc(env, i * sizeof(char *));
 	if (new_env == NULL)
 	{
-		write(stderr, "Failed to reallocate memory ", 28);
-		write(stderr, "while unsetting environment variable.\n", 38);
+		write(STDERR_FILENO, "Failed to reallocate memory ", 28);
+		write(STDERR_FILENO, "while unsetting environment variable.\n", 38);
 		return ;
 	}
 	*envp = new_env;
