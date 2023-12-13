3994571880feea2d9afe4de422616c011a65a7dc
Mini <wndavey@hotmail.com>
2023-12-13 19:57:43 +1030

    add cloexec to fd opening

--- src/parse/token_actions.c
+++ src/parse/token_actions.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 16:21:34 by wdavey            #+#    #+#             */
-/*   Updated: 2023/12/05 16:37:38 by wdavey           ###   ########.fr       */
+/*   Updated: 2023/12/13 19:57:11 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ size_t	token_1lt(char **args, t_command *command)
 {
 	if (STDIN_FILENO != command->fd[FD_IN])
 		close(command->fd[FD_IN]);
-	command->fd[FD_IN] = open(args[1], O_RDONLY);
+	command->fd[FD_IN] = open(args[1], O_RDONLY | O_CLOEXEC);
 	if (0 > command->fd[FD_IN])
 		return (-1);
 	return (1);
@@ -38,7 +38,8 @@ size_t	token_2gt(char **args, t_command *command)
 {
 	if (STDOUT_FILENO != command->fd[FD_OUT])
 		close(command->fd[FD_OUT]);
-	command->fd[FD_OUT] = open(args[1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+	command->fd[FD_OUT] = open(args[1], O_WRONLY | O_CREAT | O_APPEND
+			| O_CLOEXEC, 0644);
 	if (0 > command->fd[FD_OUT])
 		return (-1);
 	return (1);
@@ -48,7 +49,7 @@ size_t	token_1gt(char **args, t_command *command)
 {
 	if (STDOUT_FILENO != command->fd[FD_OUT])
 		close(command->fd[FD_OUT]);
-	command->fd[FD_OUT] = open(args[1], O_WRONLY | O_CREAT, 0644);
+	command->fd[FD_OUT] = open(args[1], O_WRONLY | O_CREAT | O_CLOEXEC, 0644);
 	if (0 > command->fd[FD_OUT])
 		return (-1);
 	return (1);
