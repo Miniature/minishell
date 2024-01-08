3a0ed038560d5a4725a5ddd7155c1f9cdf91dbd1
Mini <wndavey@hotmail.com>
2024-01-08 14:55:19 +1030

    remove comments, extra global def from main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 18:10:56 by wdavey            #+#    #+#             */
-/*   Updated: 2024/01/08 14:06:17 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/01/08 14:54:37 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@
 #include "parse.h"
 #include "readline/readline.h"
 #include "readline/history.h"
-
-int	g_signal;
 
 int	engine(char ***envp);
 
@@ -47,76 +45,6 @@ static char	**copy_envp(char **envp)
 	return (copy);
 }
 
-/*
-void	main_debug(int argc, char **argv, char ***envp)
-{
-
-	(void)argc;
-	(void)argv;
-	printf("%s\n", ms_getenv(envp, "PATH"));
-	ms_setenv(envp, ft_strdup("PATH=NONE"));
-	printf("%s\n", ms_getenv(envp, "PATH"));
-	printf("%s\n", ms_getenv(envp, "ASDASD"));
-	ms_setenv(envp, ft_strdup("ASDASD=NONE"));
-	printf("%s\n", ms_getenv(envp, "ASDASD"));
-	exec_command((t_command){(char *[]){"unset", "ASDASD", NULL}, envp,
-	{0, 1}});
-	printf("%s\n", ms_getenv(envp, "ASDASD"));
-	char	**tokens;
-	size_t	iii;
-	printf("tokenize\n");
-	tokens = tokenize_input("arg0 $PATH $ASDASD \"arg2 arg2 arg2\" arg3>arg5.tmp",
-			envp);
-	iii = -1;
-	while (NULL != tokens[++iii])
-	{
-		printf("%lu %s\n", iii, tokens[iii]);
-	}
-	printf("parse\n");
-	t_list	*cmds = build_commands(tokens, envp);
-	iii = -1;
-	t_command	*cmd = (t_command*)cmds->content;
-	while (NULL != ((t_command *)cmds->content)->argv[++iii])
-	{
-		char	*arg = cmd->argv[iii];
-		(void)arg;
-		printf("%lu %s\n", iii, ((t_command *)cmds->content)->argv[iii]);
-		fflush(stdout);
-	}
-	ft_lstclear(&cmds, (void(*)(void *))&command_free);
-	free_all(tokens);
-	tokens = tokenize_input("echo \"test string->exec chain\"", envp);
-	cmds = build_commands(tokens, envp);
-	exec_command(*(t_command *)cmds->content);
-	free_all(tokens);
-	ft_lstclear(&cmds, (void(*)(void *))&command_free);
-	exec_command((t_command)
-	{
-		(char *[]){
-			"echo", "dshfjsd", "aaaaaa", NULL
-		},
-		envp,
-		{0, 1}
-	});
-	exec_command((t_command)
-	{
-		(char *[]){
-			"export", ft_strdup("TESTAHSJ=TEST"), NULL
-		},
-		envp,
-		{0, 1}
-	});
-	exec_command((t_command)
-	{
-		(char *[]){
-			"env", NULL
-		},
-		envp,
-		{0, 1}
-	});
-}
-*/
-
 int	main(int argc, char **argv, char **envp)
 {
 	char	*shlvl;
@@ -135,10 +63,8 @@ int	main(int argc, char **argv, char **envp)
 				shlvl)->cstr);
 		free(shlvl);
 	}
-	//ms_setenv(&env, "exit=0");
 	signal(SIGINT, signal_handler);
 	signal(SIGQUIT, signal_handler);
-	//main_debug(argc, argv, &env);
 	engine(&env);
 	free_all(env);
 	rl_clear_history();
