364896de5d930645d5142db80d6464cca42a8461
Mini <wndavey@hotmail.com>
2024-01-08 15:48:01 +1030

    export with no args prints extra before each line

--- src/r_builtin/r_builtin_export.c
+++ src/r_builtin/r_builtin_export.c
@@ -6,13 +6,16 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 19:00:57 by wdavey            #+#    #+#             */
-/*   Updated: 2024/01/08 15:13:03 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/01/08 15:40:32 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "r_builtin.h"
 #include "env.h"
 #include "libft.h"
+#include <unistd.h>
+
+#define DCLRSTR "declare -x "
 
 int	builtin_export(t_command cmd)
 {
@@ -23,7 +26,11 @@ int	builtin_export(t_command cmd)
 		i = 0;
 		while ((*cmd.envp)[i])
 		{
-			ft_putendl_fd((*cmd.envp)[i], cmd.fd[FD_OUT]);
+			if ((*cmd.envp)[i][0] != '?')
+			{
+				write(cmd.fd[FD_OUT], DCLRSTR, ft_strlen(DCLRSTR));
+				ft_putendl_fd((*cmd.envp)[i], cmd.fd[FD_OUT]);
+			}
 			i++;
 		}
 	}
