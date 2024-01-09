eed0569505a7bc6db911153093f8233204e774d1
Mini <wndavey@hotmail.com>
2024-01-09 15:40:36 +1030

    redo exit builtin

--- src/r_builtin/r_builtin_exit.c
+++ src/r_builtin/r_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: wdavey <wdavey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 19:00:54 by wdavey            #+#    #+#             */
-/*   Updated: 2023/12/13 19:00:54 by wdavey           ###   ########.fr       */
+/*   Updated: 2024/01/09 15:38:10 by wdavey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,27 @@
 #include <signal.h>
 #include "r_signal.h"
 
+#define NONNUMERR_START "minishell: exit: "
+#define NONNUMERR_END ": numeric argument required\n"
+
 int	builtin_exit(t_command cmd)
 {
-	char	*string;
-
-	string = malloc(6);
-	string = "exit=";
 	if (cmd.argv[1] == NULL)
 	{
-		ms_setenv(cmd.envp, ft_strjoin(string, "1"));
-		write(cmd.fd[FD_OUT], "exit\n", 5);
+		return (0);
 	}
-	else if (cmd.argv[3] == NULL)
+	else if (cmd.argv[2] == NULL)
 	{
-		ms_setenv(cmd.envp, ft_strjoin(string, cmd.argv[1]));
+		if (str_has_all(cmd.argv[1], (bool (*)(char))&ft_isalnum)
+			&& !str_has_any(cmd.argv[1], (bool (*)(char))&ft_isalpha))
+			return (ft_atoi(cmd.argv[1]));
+		write(cmd.fd[FD_OUT], NONNUMERR_START, ft_strlen(NONNUMERR_START));
 		write(cmd.fd[FD_OUT], cmd.argv[1], ft_strlen(cmd.argv[1]));
+		write(cmd.fd[FD_OUT], NONNUMERR_END, ft_strlen(NONNUMERR_END));
 	}
 	else
 	{
 		write(cmd.fd[FD_OUT], "exit: too many arguments\n", 25);
 	}
-	free(string);
 	return (0);
 }
